af32aaa79478eb356167c8034a4c18a62285d836
Sumanjeet <sumanjeet0012@gmail.com>
2024-05-08 04:29:55 +0530

    beforeEach is used

--- cypress/e2e/fundamentals.cy.js
+++ cypress/e2e/fundamentals.cy.js
@@ -1,13 +1,15 @@
 describe('Fundamentals test', () => {
-
-  it('Contains correct header text', () => {
+  beforeEach(() => {
     cy.visit('/fundamentals')
+  })
+  it('Contains correct header text', () => {
+    // cy.visit('/fundamentals')
     cy.get('[data-test="fundamentals headers"]').contains(/Testing Fundamentals/i)
     cy.get('[data-test="fundamentals headers"]').should('contain.text', 'Testing Fundamentals' )
   })
 
   it('Accordium works correctly', () => {
-    cy.visit('/fundamentals')
+    // cy.visit('/fundamentals')
     cy.contains(/Your tests will exist in a describe block/i).should('not.be.visible')
     cy.get('[data-test="accordium-item-1"] div[role="button"]').click()
     cy.contains(/Your tests will exist in a describe block/i).should('be.visible')
