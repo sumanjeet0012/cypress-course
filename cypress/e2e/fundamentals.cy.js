4fbcfa5d2218749222c269fb626dd20dd6e5b635
Sumanjeet <sumanjeet0012@gmail.com>
2024-05-08 04:45:02 +0530

    added custom commands

--- cypress/e2e/fundamentals.cy.js
+++ cypress/e2e/fundamentals.cy.js
@@ -4,7 +4,8 @@ describe('Fundamentals test', () => {
   })
   it('Contains correct header text', () => {
     // cy.visit('/fundamentals')
-    cy.get('[data-test="fundamentals headers"]').contains(/Testing Fundamentals/i)
+    // cy.get('[data-test="fundamentals headers"]').contains(/Testing Fundamentals/i)
+    cy.getDataTest('fundamentals headers').contains(/Testing Fundamentals/i)
     cy.get('[data-test="fundamentals headers"]').should('contain.text', 'Testing Fundamentals' )
   })
 
