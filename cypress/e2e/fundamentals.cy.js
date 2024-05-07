77518ca47d6f6d6104053ce64c1700ff39272107
Sumanjeet <sumanjeet0012@gmail.com>
2024-05-08 04:29:20 +0530

    command chaining and assertion

--- cypress/e2e/fundamentals.cy.js
+++ cypress/e2e/fundamentals.cy.js
@@ -1,7 +1,17 @@
 describe('Fundamentals test', () => {
+
   it('Contains correct header text', () => {
     cy.visit('/fundamentals')
     cy.get('[data-test="fundamentals headers"]').contains(/Testing Fundamentals/i)
     cy.get('[data-test="fundamentals headers"]').should('contain.text', 'Testing Fundamentals' )
+  })
+
+  it('Accordium works correctly', () => {
+    cy.visit('/fundamentals')
+    cy.contains(/Your tests will exist in a describe block/i).should('not.be.visible')
+    cy.get('[data-test="accordium-item-1"] div[role="button"]').click()
+    cy.contains(/Your tests will exist in a describe block/i).should('be.visible')
+    cy.get('[data-test="accordium-item-1"] div[role="button"]').click()
+    cy.contains(/Your tests will exist in a describe block/i).should('not.be.visible')
   })
 })
