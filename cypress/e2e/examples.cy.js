57aa3f2e35869ed98d6a0d500c170f1edde173fe
Sumanjeet <sumanjeet0012@gmail.com>
2024-05-09 23:53:57 +0530

    multi page testing

---
+++ cypress/e2e/examples.cy.js
@@ -0,0 +1,31 @@
+describe("Examples test", () => {
+  beforeEach(() => {
+    cy.visit("/examples");
+  });
+  it("Multi page testing", () => {
+    cy.contains(/Examples/i);
+
+    cy.getDataTest("nav-why-cypress").click(); 
+    cy.location("pathname").should("eq", "/"); 
+
+    cy.getDataTest("nav-overview").click();
+    cy.location("pathname").should("eq", "/overview");
+
+    cy.getDataTest("nav-fundamentals").click();
+    cy.location("pathname").should("eq", "/fundamentals");
+
+    cy.getDataTest("nav-forms").click();
+    cy.location("pathname").should("eq", "/forms");
+
+    cy.getDataTest("nav-examples").click();
+    cy.location("pathname").should("eq", "/examples");
+
+    cy.getDataTest("nav-component").click();
+    cy.location("pathname").should("eq", "/component");
+
+    cy.getDataTest("nav-best-practices").click();
+    cy.location("pathname").should("eq", "/best-practices");
+  });
+
+  
+});
